c34e9cc5f1d318e600e678993c6825b29c02e2bb
Azhar Rivaldi <46733833+AzharRivaldi@users.noreply.github.com>
2021-03-23 11:03:37 +0700

    Update ApiEndpoint.kt

--- app/src/main/java/com/azhar/kodepos/networking/ApiEndpoint.kt
+++ app/src/main/java/com/azhar/kodepos/networking/ApiEndpoint.kt
@@ -6,4 +6,6 @@ package com.azhar.kodepos.networking
 
 object ApiEndpoint {
     val BASEURL = "https://kodepos.now.sh/search?q={query}"
+    
+    //or val BASEURL = "https://kodepos.herokuapp.com/search?q={query}"
 }
